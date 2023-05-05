0b0ff0f0b346540ed9687c2269420324973d411d
Devendra Bhattarai <91841677+Deven2057@users.noreply.github.com>
2023-05-05 10:15:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log(document.body.firstChild);
+console.log(document.body.lastChild);
+
+let a= document.body.firstChild
+console.log(a.parentNode)
+console.log(a.nextSibling)
+console.log(a.previousSibling)
+console.log(a.firstChild.nextSibling)
